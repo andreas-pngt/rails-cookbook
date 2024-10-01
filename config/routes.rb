3ea3095ab65f55b1b8fadebaa4363ba67ae93e38
Andreas Panagiotou <andrewpngt@hotmail.com>
2024-10-01 13:08:14 +0100

    Added Navbar,Banner,List of Categories Cards

--- config/routes.rb
+++ config/routes.rb
@@ -11,8 +11,10 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  root "categories#index"
   resources :categories, except: [:edit, :update] do
     resources :bookmarks, only: [:new, :create]
   end
+
   resources :bookmarks, only: [:destroy]
 end
